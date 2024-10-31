a1b658e569a3784a11d6873ebf843fffba37efa9
Anamaría Soriano <anasperez10@gmail.com>
2024-10-31 16:23:52 -0500

    Road Movment

---
+++ Assets/Scripts/SectionTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionTrigger : MonoBehaviour
+{
+
+    public GameObject road;
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Instantiate(road, new Vector3(0, 0, 27), Quaternion.identity);
+        }
+        
+    }
+}
