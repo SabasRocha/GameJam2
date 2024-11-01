b080e5ea1f556f611a28a33c7a70b1da57b8931f
marvisa22 <107163520+marvisa22@users.noreply.github.com>
2024-10-31 22:32:10 -0500

    primera edicion
    Se crea la escena Diseño_interfaz
    Se crea el sistema de puntaje y los canvas de puntaje y distancia
    se crean scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    private GameUIManager uiManager;
     // Start is called before the first frame update
     void Start()
+    {
+        uiManager = FindObjectOfType<GameUIManager>();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // se añade una función para detectar colisiones con objetos etiquetados como "Obstacle".
+    // asegurar de que los objetos obstáculos tengan el tag "Obstacle"
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Obstacle"))
+        {
+            if (uiManager != null)
+            {
+                uiManager.ShowGameOver();
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Debug.LogError("GameUIManager no asignado en PlayerMovement.");
+            }
+        }
     }
 }
