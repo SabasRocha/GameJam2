160349dac41214682faac355121d661f99ca14e8
marvisa22 <107163520+marvisa22@users.noreply.github.com>
2024-11-03 12:30:59 -0500

    Sincronizacion
    Se sincroniza la barra de vida con el tiempo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    private GameUIManager uiManager;
+   
     // Start is called before the first frame update
     void Start()
     {
-        uiManager = FindObjectOfType<GameUIManager>();
+        
     }
 
     
@@ -17,22 +16,5 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
-
-    // se añade una función para detectar colisiones con objetos etiquetados como "Obstacle".
-    // asegurar de que los objetos obstáculos tengan el tag "Obstacle"
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Obstacle"))
-        {
-            if (uiManager != null)
-            {
-                uiManager.ShowGameOver();
-                Time.timeScale = 0;
-            }
-            else
-            {
-                Debug.LogError("GameUIManager no asignado en PlayerMovement.");
-            }
-        }
-    }
+    
 }
